c31c66c8a22b5c066c39e5380b9b007bf71eca96
wade xing <xingxing@tianji.com>
2015-11-25 17:23:41 +0800

    15章第二小节

--- code/ch15/VCard.hs
+++ code/ch15/VCard.hs
@@ -23,10 +23,46 @@ onePersonalPhone ps = case lookup Home ps of
 
 -- file: ch15/VCard.hs
 
-allPersonalPhone :: [(Context, Phone)] -> Phone
-allPersonalPhone ps = map snd number
+allPersonalPhones :: [(Context, Phone)] -> [Phone]
+allPersonalPhones ps = map snd numbers
     where numbers = case filter (contextIs Business) ps of
                       [] -> filter (contextIs Mobile) ps
                       ns -> ns
 
 contextIs a (b, _) = a == b
+              
+-- file: ch15/VCard.hs
+class Monad m => MonadPlus m where
+    mzero :: m a
+    mplus :: m a -> m a -> m a
+
+-- file: ch15/VCard.hs
+instance MonadPlus [] where
+    mzero = []
+    mplus = (++)
+
+instance MonadPlus Maybe where
+    mzero = Nothing
+
+    Nothing `mplus` ys = ys
+    xs      `mplus` _  = xs 
+
+-- file: ch15/VCard.hs
+oneBussinessPhone :: [(Context, Phone)] -> Maybe Phone
+oneBussinessPhone ps = lookup Business ps `mplus` lookup Mobile ps
+
+allPersonalPhones :: [(Context, Phone)] -> [Phone]
+allPersonalPhones ps = map snd $ filter (contextIs Home) ps `mplus`
+                                  filter (contextIs Mobile) ps
+
+-- file: ch15/VCard.hs
+lookup :: (Eq a) => a -> [(a, b)] -> Maybe b
+lookup _ []                       = Nothing
+lookup k ((x,y):xys) | x == k     = Just y
+                      | otherwise  = lookup k xys
+
+-- file: ch15/VCard.hs
+lookup :: (Eq a) => a -> [(a, b)] -> Maybe b
+lookup _ []        =  Nothing
+lookup k ((x,y):xys) | x == k = Just y
+                     | otherwise = lookup k xys
